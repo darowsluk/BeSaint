7e1c24083cbfeac0ad144ab6439f1852dfb1ce36
Łukasz Darowski <lukasz.darowski@gmail.com>
2019-01-01 16:04:42 +0100

    Created SignActivity view and onStartClick() in MainActivity

--- app/src/main/java/org/sds/besaint/MainActivity.java
+++ app/src/main/java/org/sds/besaint/MainActivity.java
@@ -1,7 +1,9 @@
 package org.sds.besaint;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onStartClick(View view) {
+        Intent intent = new Intent(this, SignActivity.class);
+        startActivity(intent);
+    }
 }
+
