891fe29cf824d80d6b7e3c520ccd8df29c512a0c
Łukasz Darowski <besaintsds@gmail.com>
2019-01-02 17:01:17 +0100

    Created JourneyActivity

--- app/src/main/java/org/sds/besaint/SignActivity.java
+++ app/src/main/java/org/sds/besaint/SignActivity.java
@@ -1,7 +1,9 @@
 package org.sds.besaint;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class SignActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign);
+    }
+
+    public void onOKClick(View view) {
+        Intent intent = new Intent(this, JourneyActivity.class);
+        startActivity(intent);
     }
 }
